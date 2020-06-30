5326217f319d7155fcf60df42425075a2bd49170
copiedcode <raphael.webdesign@outlook.com>
2020-06-30 17:22:08 +0200

    build getFunction with API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
+const App = () => {
+
+  const APP_ID = 'd2339bbf';
+  const APP_KEY = '031ceed39e60a0a66da5226abf482345';
+
+
+useEffect(() => {
+  getRecipes();
+}, []);
+
+const getRecipes = async () => {
+ const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+ const data = await response.json();
+ console.log(data);
+}
+
+  return(
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
