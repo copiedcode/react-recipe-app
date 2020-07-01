f7c6aeb1d3586974f126ef9e3cf3771174ade4cf
copiedcode <raphael.webdesign@outlook.com>
2020-07-01 09:51:02 +0200

    start making everything more beautiful

--- src/App.js
+++ src/App.js
@@ -36,20 +36,23 @@ const getSearch = e => {
 
   return(
     <div className="App">
-      <form className="search-form" onSubmit={getSearch}>
-        <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
-        <button className="search-button" type="submit">
-          Search
-        </button>
-      </form>
-
+          <form className="search-form" onSubmit={getSearch}>
+            <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
+            <button className="search-button" type="submit">
+              Search
+            </button>
+          </form>
+        <div className="recipes">
       {recipes.map(recipe => (
         <Recipe
           key={recipe.recipe.label} 
           title={recipe.recipe.label} 
           calories={recipe.recipe.calories} 
-          image={recipe.recipe.image}/>
+          image={recipe.recipe.image}
+          ingredients={recipe.recipe.ingredients}
+        />
       ))}
+        </div>
     </div>
   )
 }
